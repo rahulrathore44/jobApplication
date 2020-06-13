7483050dc50fe694175464bb1323073ac36f71a7
rathr1 <rathr1@L33986WUS>
2020-06-13 09:13:02 -0700

    Change the jwt expiry date

--- src/main/java/com/lab/jobportal/filter/SecurityConstants.java
+++ src/main/java/com/lab/jobportal/filter/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.lab.jobportal.filter;
 
 public class SecurityConstants {
 	public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 72_00; // 2 hours
+    public static final long EXPIRATION_TIME = 72_00_000; // 2 Hours
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users/sign-up";
